a4ce3f3b5cbf25aa66a60877f6a9eaf0a15f9310
asphyxia <asphyxia@novusphere.io>
2018-12-13 21:09:34 +0000

    recently visited

--- src/components/core/RecentlyVisited.vue
+++ src/components/core/RecentlyVisited.vue
@@ -1,32 +1,35 @@
 <template>
     <div>
         <h3>Recently Visited</h3>
-        <div class="divline"></div>
-        <div class="blocktxt"> <a href="#">This Is Just Filler Text</a>
-        </div>
-        <div class="divline"></div>
-        <div class="blocktxt"> <a href="#">This Is Just Filler Text</a>
-        </div>
-        <div class="divline"></div>
-        <div class="blocktxt"> <a href="#">This Is Just Filler Text</a>
-        </div>
-        <div class="divline"></div>
-        <div class="blocktxt"> <a href="#">This Is Just Filler Text</a>
-        </div>
-        <div class="divline"></div>
-        <div class="blocktxt"> <a href="#">This Is Just Filler Text</a>
+        <div v-for="p in posts" :key="p.id">
+          <div class="divline"></div>
+          <div class="blocktxt"> 
+            <router-link :to="{ name: 'Thread', params: { id: p.id, sub: p.sub } }">{{ p.title }}</router-link>
+          </div>
         </div>
     </div>
 </template>
 
 <script>
+import { storage } from "@/storage";
+
 export default {
   name: "RecentlyVisited",
   async mounted() {
+    var new_posts = Object.values(storage.new_posts);
+    new_posts = new_posts
+      .filter(np => np.id)
+      .sort((s, s2) => s2.seen - s.seen)
+      .slice(0, 5)
+      .map(np => Object.assign({}, np));
+
+    this.posts = new_posts;
   },
   methods: {},
   data() {
-    return {};
+    return {
+      posts: []
+    };
   }
 };
 </script>
