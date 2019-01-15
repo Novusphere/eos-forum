f65e100ad7e507b3b5f0999d94dc243755a3b765
Code Blue <codubru@gmail.com>
2019-01-15 13:08:27 -0500

    Hide date in referendum, Removed margin-left on .post-body

--- src/components/core/Post.vue
+++ src/components/core/Post.vue
@@ -164,9 +164,10 @@
               </a>
             </li>
             <li class="list-inline-item" v-if="post.depth === 0">
-              <font-awesome-icon :icon="['fas', 'clock']" />
-              {{ new Date(post.createdAt * 1000).toLocaleString() }}
-
+              <template v-if="!post.referendum">
+                <font-awesome-icon :icon="['fas', 'clock']" />
+                {{ new Date(post.createdAt * 1000).toLocaleString() }}
+              </template>
               <router-link
                 @click.stop
                 v-if="post.id && is_edit"
@@ -596,7 +597,7 @@ export default {
   height: 25px!important;
   width: 25px!important;
 }
-.post-body, .date {
+.date {
   margin-left: 15px;
 }
 .tip-amount {
