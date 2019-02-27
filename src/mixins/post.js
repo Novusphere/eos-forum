f4d275431cad8b03575b3ec0e138d0f7864d873d
Todd Kao <todd@unoapp.com>
2019-02-27 13:31:52 -0500

    Return to correct url after closing modal

--- src/mixins/post.js
+++ src/mixins/post.js
@@ -2,11 +2,12 @@ export default {
   methods: {
     $_openPost (postID, sub, title){
       this.selectedPostID = postID;
+      this.oldRoute = this.$route.path;
       history.pushState({},"",`${window.__PRE_ROUTE__}/e/${sub}/${postID}/${title}`);
     },
     $_closePost () {
       this.selectedPostID = undefined;
-      history.pushState({},"",`${window.__PRE_ROUTE__}/`);
+      history.pushState({},"",`${window.__PRE_ROUTE__}${this.oldRoute}`);
     },
   }
 }
