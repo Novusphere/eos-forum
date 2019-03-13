724569fbfe338e306f62f7fad51df590d418f0f7
Todd Kao <todd@unoapp.com>
2019-03-13 15:25:18 -0400

    Fixes to SetID and set sub names to lowercase
    Remove caps on subs
    Use ecc.isValidPrivate to validate key
    Replace Set ID with anon_id.name if validPrivate 
    Update 'login' to 'Login'

--- src/components/section/Layout.vue
+++ src/components/section/Layout.vue
@@ -23,13 +23,13 @@
             <div class="d-sm-inline-block">
               <b-dropdown variant="link" :text="$route.params.sub || 'Home'" class="mobile">
                 <b-dropdown-item @click="$router.push({name: 'Index'})">
-                  Home
+                  home
                 </b-dropdown-item>
                 <b-dropdown-item @click="$router.push({name: 'Sub', params: { sub: 'all' } })">
-                  All
+                  all
                 </b-dropdown-item>
                 <b-dropdown-item @click="$router.push({name: 'Sub', params: { sub: 'referendum' } })">
-                  Referendum 
+                  referendum
                 </b-dropdown-item>
                 <b-dropdown-item
                   v-for="s in subs()"
@@ -46,7 +46,7 @@
                 </b-dropdown-item>
               </b-dropdown>
               <div class="desktop sub">
-                {{ $route.params.sub || 'Home' }}
+                {{ $route.params.sub || 'home' }}
               </div>
             </div>
           </div>
@@ -55,11 +55,11 @@
             <template v-if="!identity.account">
               <button class="btn btn-primary mx-2 ConnectButton"
                 @click="login()">
-                login
+                Login
               </button>
 
               <button @click="$router.push({name: 'setID'})" class="btn btn-primary mx-2 ConnectButton">
-                Set ID
+                {{ validAnonID() ? anon_id.name : 'Set ID' }}
               </button>
             </template>
 
@@ -131,16 +131,16 @@
             <div class="desktop block">
               <router-link class="dropdown-item"
                 :to="{name: 'Index' }">
-                Home
+                home
               </router-link>
               <router-link class="dropdown-item"
                 :to="{name: 'Sub', params: { sub: 'all' } }">
-                All
+                all
               </router-link>
               <router-link v-if="eos_referendum"
                 class="dropdown-item"
                 :to="{name: 'Sub', params: { sub: 'referendum' } }">
-                  Referendum
+                  referendum
               </router-link>
               <div class="divline" />
               <router-link v-for="s in subs()"
@@ -278,6 +278,7 @@
 import "@/assets/css/style2.css";
 import "@/assets/css/custom.css";
 
+import ecc from "eosjs-ecc";
 import ui from "@/ui";
 import { BRANDS, FORUM_BRAND } from "@/ui/constants";
 import { storage } from "@/storage";
@@ -321,6 +322,7 @@ export default {
   },
   async mounted() {
     this.updateBrand();
+    this.anon_id = storage.anon_id;
     this.identity = await GetIdentity();
     window.addEventListener("identity", this.updateIdentity);
   },
@@ -329,7 +331,7 @@ export default {
   },
   methods: {
     validAnonID() {
-      return storage.anon_id.name !== '';
+      return ecc.isValidPrivate(this.anon_id.key);
     },
     is_subscribed() {
       return this.subscribed_subs.includes(this.$route.params.sub);
@@ -389,6 +391,7 @@ export default {
     return {
       eos_referendum: storage.eos_referendum,
       identity: {},
+      anon_id: {},
       brand_logo: "",
       brand_icon: "",
       brand_symbol: "",
@@ -426,7 +429,6 @@ export default {
   overflow: auto;
 }
 .mobile .dropdown-toggle {
-  text-transform: capitalize;
   font-size: 18px;
   color: black;
   padding: 0px;
@@ -438,7 +440,6 @@ export default {
   justify-content:space-between;
 }
 .sub {
-  text-transform: capitalize;
   font-size: 18px;
   color: black;
 }
