63565a97942f0c70cd8cb1724fa4c651d3305ee3
lakeprojectnv <nikunj@lakeproject.co>
2019-05-09 01:05:45 -0400

    (FEAT) Facebook attachments

--- src/types/post/post.js
+++ src/types/post/post.js
@@ -561,7 +561,6 @@ class Post {
                 handle: (m) => attach(m[0], 'url', 'link')
             },
             {
-                
                 match: /https:\/\/medium.com\/@[\w]+\/.+/i,
                 handle: (m) => attach(m[0], 'url', 'link')
             },
@@ -572,6 +571,22 @@ class Post {
             {
                 match: /https:\/\/www.instagr.am(\/[a-zA-Z0-9-_]+)?\/p\/[a-zA-Z0-9-_]+(\/?.+)?/i,
                 handle: (m) => attach(m[0], 'url', 'link')
+            },
+          /**
+           * There are many different combinations of facebook URLs:
+           * facebook.com/video.php?###
+           * facebook.com/stories.php?###
+           * facebook.com/photo.php?###
+           * facebook.com/######/videos/###
+           * facebook.com/######/posts/###
+           *
+           * All of the above are also in combination with fb.me.
+           * Perhaps it is best to generalize to just the host(s).
+           * Old regex: ^(https?:\/\/)?(www\.)?(facebook|fb).(com|me)(\/[a-zA-Z0-9(.?)]+\/(posts|videos)\/[a-zA-Z0-9(.?)]+)
+           */
+          {
+              match: /^(https?:\/\/)?(www\.)?(facebook|fb).(com|me)/i,
+              handle: (m) => attach(m[0], 'url', 'link')
             }
         ];
 
